aa492b673daedaac0dca20ec005bfe76a5538ae9
sminnaar <sminnaar@student.wethinkcode.co.za>
2019-07-12 17:00:38 +0200

    Mmmmm...

--- ft_ls_rec.c
+++ ft_ls_rec.c
@@ -1,26 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_ls_rec.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sminnaar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/08 13:42:38 by sminnaar          #+#    #+#             */
-/*   Updated: 2019/07/12 14:17:15 by sminnaar         ###   ########.fr       */
+/*   Created: 2019/07/12 14:06:47 by sminnaar          #+#    #+#             */
+/*   Updated: 2019/07/12 14:27:50 by sminnaar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/includes/libft.h"
 #include <dirent.h>
 
-DIR *ft_ls_rec(DIR *direct);
-
-int	main()
+DIR	*ft_ls_rec(DIR *direct)
 {
-	DIR	*dirp;
+	DIR				*cur;
+	struct dirent	*dp;
 
-	dirp = opendir("./Test");
-	ft_ls_rec(dirp);
+	cur = opendir(".");
+	if (cur == NULL)
+		perror("Can't open dir");
+	else
+	{
+		while (cur != NULL)
+		{
+			dp = readdir(cur);
+			if (dp == NULL)
+				break ;
+			ft_putendl(dp->d_name);
+			if ((strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..")))
+			{
+			ft_ls_rec(++cur);
+			if (cur == NULL)
+				break ;
+			}
+		}
+	}
+	closedir(cur);
+	return (cur);
 }
-
-
